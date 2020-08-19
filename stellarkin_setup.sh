b180024c5e856a3bee038b2f98306ac67f829084
Josh Davidson <jdavidson@621.solutions>
2020-08-19 16:10:02 -0400

    Fixing paths and adding -y to apt installs

--- stellarkin_setup.sh
+++ stellarkin_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install pre-requisite packages
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install raincat gamine pysycache childsplay gcompris tictactoe-ng klettres ktuberling khangman kanagram tanglet marble kgeography mah-jong lagan freecell-solver-bin gnome-mastermind kigo gnome-chess aisleriot tali mypaint* tuxpaint kstars kwordquiz scratch tuxmath kdeedu animals atomix basic256 gamine granule pysiogame tuxtype xball stellarium libreoffice gnome-dictionary gnome-breakout
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y raincat gamine pysycache childsplay gcompris tictactoe-ng klettres ktuberling khangman kanagram tanglet marble kgeography mah-jong lagan freecell-solver-bin gnome-mastermind kigo gnome-chess aisleriot tali mypaint* tuxpaint kstars kwordquiz scratch tuxmath kdeedu animals atomix basic256 gamine granule pysiogame tuxtype xball stellarium libreoffice gnome-dictionary gnome-breakout
 
 # Move over the custom splash image
-sudo cp ../resources/splash.png /usr/share/plymouth/themes/pix/
+sudo cp ./resources/splash.png /usr/share/plymouth/themes/pix/
 
 # Desktop/Menu customizations
-cp -R ../config/* ~/.config/
-cp -R ../desktop/* ~/Desktop
+cp -R ./config/* ~/.config/
+cp -R ./desktop/* ~/Desktop
 
 # Rename host
 sudo hostnamectl set-hostname stellarkin
