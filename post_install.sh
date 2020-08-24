96e9ba25d5d2f5c6cad9bc3d47d657b0ff4882ee
Josh Davidson <josh.davidson@gmail.com>
2020-08-24 13:29:18 -0400

    Fixes to post installation scripts

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # This script is for stellarkin distro.  Installs packages on startup to reduce the size of the img
+wget -q --tries=10 --timeout=200 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+  echo "Installing Stellarium and Tuxpaint on first boot...This may take some time."
+  sudo apt-get install -y stellarium tuxpaint && \
+  rm /home/pi/.config/stellarkin/post_install.sh && \
+  rm /home/pi/.config/autostart/run-once.desktop
+else
+  echo "You appear to be offline.  Please configure networking and reboot to complete installation..."
+  sleep 15
+fi
 
-echo "Installing Stellarium and Tuxpaint on first boot...This may take some time."
-sudo apt-get install -y stellarium tuxpaint && rm /home/pi/.config/stellarkin/post_install.sh
 
--- run_once.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script is for stellarkin distro.  Installs packages on startup to reduce the size of the img
-
-lxterminal -e /home/pi/.config/stellarkin/post_install.sh
-sudo rm /etc/profile.d/run_once.sh'
-
