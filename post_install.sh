498a0876483fe2831415ba01b228616fc1974c6b
Josh Davidson <josh.davidson@gmail.com>
2020-08-24 12:04:36 -0400

    Updates to scripts for post install of tuxpaint and stellarium

--- post_install.sh
+++ post_install.sh
@@ -2,5 +2,5 @@
 # This script is for stellarkin distro.  Installs packages on startup to reduce the size of the img
 
 echo "Installing Stellarium and Tuxpaint on first boot...This may take some time."
-sudo apt-get install -y stellarium tuxpaint && rm /home/pi/post_install_tasks.sh
+sudo apt-get install -y stellarium tuxpaint && rm /home/pi/.config/stellarkin/post_install.sh
 
--- run_once.sh
+++ run_once.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is for stellarkin distro.  Installs packages on startup to reduce the size of the img
 
-lxterminal -e /home/pi/install_big_packages.sh
+lxterminal -e /home/pi/.config/stellarkin/post_install.sh
 sudo rm /etc/profile.d/run_once.sh'
 
